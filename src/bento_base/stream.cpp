29f692f7bceb0684675453fda6a9cddcdc60747a
abenyoub <Benyoub.anis@gmail.com>
2021-07-11 18:06:22 +0200

    fix on linux

--- src/bento_base/stream.cpp
+++ src/bento_base/stream.cpp
@@ -1,5 +1,6 @@
 // SDK includes
 #include "bento_base/stream.h"
+#include <cstring>
 
 namespace bento {
 	void pack_buffer(Vector<char>& buffer, uint32_t write_size, const char* data)
@@ -7,14 +8,14 @@ namespace bento {
 		if (write_size) {
 			uint32_t old_size = buffer.size();
 			buffer.resize(old_size + write_size);
-			memcpy(buffer.begin() + old_size, data, write_size);
+			std::memcpy(buffer.begin() + old_size, data, write_size);
 		}
 	}
 
 	void unpack_buffer(const char*& stream, uint32_t read_size, char* data)
 	{
 		if (read_size) {
-			memcpy(data, stream, read_size);
+			std::memcpy(data, stream, read_size);
 			stream += read_size;
 		}
 	}
